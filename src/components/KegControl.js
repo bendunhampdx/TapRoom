2ab953d3f6a58e7bc68676e0fb5d2674995dbb32
bendunhampdx <bendunhampdx@gmail.com>
2021-11-19 14:05:01 -0800

    Update Keg Control/State

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,5 +1,30 @@
 import React from "react";
 import KegList from "./KegList";
+import NewKegForm from "./NewKegForm";
+import EditKegForm from "./EditKegForm";
+import KegDetail from "./KegDetail";
 
+class KegControl extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        formVisibleOnPage: false,
+        kegList: [],
+        selectedKeg: null,
+        editing: false
+    };
+  }
+
+
+
+
+
+
+
+
+
+
+}
 
 export default KegControl;  
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,1 +1,57 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <label htmlFor="name">Name:</label>
+        <input
+          type='text'
+          name='name'
+          placeholder='Beer Name'
+          // value={props.formData.name}
+          // onChange={props.formChangeHandler}
+        />
+        <label htmlFor="brand">Brand:</label>
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand'
+          // value={props.formData.brand}
+          // onChange={props.formChangeHandler}
+        />
+        <label htmlFor="price">Price:</label>
+        <input
+          type='text'
+          name='price'
+          placeholder='Price'
+          // value={props.formData.price}
+          // onChange={props.formChangeHandler}
+        />
+        <label htmlFor="abv">Alchohol Content:</label>
+        <input
+          type='text'
+          name='alcoholContent'
+          placeholder='Alcohol Content'
+          // value={props.formData.alcoholContent}
+          // onChange={props.formChangeHandler}
+        />
+        <label htmlFor="pints">Pints left:</label>
+          <input
+            type="text"
+            name="pints"
+        />
+
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
